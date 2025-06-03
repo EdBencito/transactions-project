ed7f2df1520c75bed21b5a5529a95ece061521e5
Ed Bencito <RusselBencito@gmail.com>
2025-06-03 02:36:06 +0100

    * stopped autoconfigure datasource for modules that don't connect to Database
    * added README.md

--- fraud-detection-service/src/main/java/dev/ed/fraud_detection_service/FraudDetectionServiceApplication.java
+++ fraud-detection-service/src/main/java/dev/ed/fraud_detection_service/FraudDetectionServiceApplication.java
@@ -2,8 +2,9 @@ package dev.ed.fraud_detection_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FraudDetectionServiceApplication {
 
 	public static void main(String[] args) {
